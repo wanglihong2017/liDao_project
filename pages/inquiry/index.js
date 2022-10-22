f4985651772dd54afcd7f718d37940decaa60a52
wanglihong <3243922776@qq.com>
2022-10-22 21:52:40 +0800

    【需求开发:在线问诊需求开发10-22】

--- pages/inquiry/index.js
+++ pages/inquiry/index.js
@@ -1,5 +1,5 @@
 // pages/inquiry/index.js
-import {api_onlineAsk} from '../../utils/api'
+import {api_onlineAsk,api_giveUp} from '../../utils/api'
 const app = getApp();
 Page({
 
@@ -47,6 +47,30 @@ Page({
         url: '/pages/logo/index'
       })
     }
+  },
+  giveUpBtns(e){
+    let getData = e.currentTarget.dataset.item 
+    let params = {
+      userId: wx.getStorageSync("userId"),
+      targetUserId: getData.userId,
+      id: getData.id,
+      articleType:getData.articleType,
+      upType: 1,
+      type: getData.isGiveUp === 1 ? 0 : 1,
+    };
+    api_giveUp(params).then((res) => {
+      let { code } = res;
+      if (code === "0") {
+          var index =  this.data.onLineData.findIndex((item)=> item.id === getData.id)
+          var isGiveUp = "onLineData[" + index + "].isGiveUp" //这里必须这样拼接
+          var giveUpQty = "onLineData[" + index + "].giveUpQty"
+          var giveUpQtyNum = this.data.onLineData[index].giveUpQty
+          this.setData({ //异步刷新，就是渲染
+              [isGiveUp] : getData.isGiveUp === 1 ? 0 : 1, //修改值为0
+              [giveUpQty] :getData.isGiveUp === 1 ? giveUpQtyNum-1:giveUpQtyNum+1
+          })
+      }
+    });
   },
   getInquiryList(){
     let params = {
